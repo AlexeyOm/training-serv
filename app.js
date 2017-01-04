7adcb95f1f99b489f662d603ce29af161ae3c484
AlexeyOm <alexey.omelchenko@gmail.com>
2017-01-04 22:30:09 +0300

    started adding auth logic

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser')
 var cors = require('cors');
+//var session = require('express-session');
 
 
 var app = express();
@@ -19,9 +20,15 @@ var app = express();
 var methodOverride = require('method-override');
 
 // all environments
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+app.use(express.cookieParser('S3CRE7'));
+app.use(express.cookieSession());
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -29,14 +36,15 @@ app.use(express.urlencoded());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use(cors());
 
 
 // create application/json parser 
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
  
 // create application/x-www-form-urlencoded parser 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // development only
 if ('development' == app.get('env')) {
@@ -46,7 +54,9 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 
-app.use('/api/users', jsonParser, function (req, res, next) {
+
+
+app.use('/api/users', loadUser, jsonParser, function (req, res, next) {
   if (!req.body) return res.sendStatus(400)
   next();//res.json(req.body.age);
 });
@@ -59,9 +69,24 @@ app.use('/api/login', jsonParser, function (req, res, next) {
 });
 app.use('/api/login', login.index);
 
-app.use('/sendmail', sendMail.index);
+//app.use('/sendmail', sendMail.index);
 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+function loadUser(req, res, next) {
+  console.log(req.query);
+
+  //TODO тут заменить на логику поиска нужного юзера
+  if (typeof req.query != 'undefined' && req.query.user_id === 'test_user') {
+        req.currentUser = req.query.user_id;
+        console.log('logged in');
+        next();
+  } else {
+  		res.jsonp('auth_required');
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -14,9 +14,9 @@ exports.list = function(req, res){
 	//   }
 	// });
 
-	//const jwork = [{reps : 6, rest : 1},{reps : 66, rest : 1},{reps : 666, rest : 1},{reps : 0, test: 1}];
+	const jwork = [{reps : 6, rest : 1},{reps : 66, rest : 1},{reps : 666, rest : 1},{reps : 0, test: 1}];
 
-	const jwork = [{reps : 6, rest : 1},{reps : 15, test: true}];
+	//const jwork = [{reps : 6, rest : 1},{reps : 15, test: true}];
 	const work = 5;
 	res.json(jwork);
   //res.send("respond with a resource");
